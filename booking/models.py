1f0868fd546d6536ff9804295ffda01475d25141
Judit3 3 <katapunpinpun@yahoo.es>
2024-02-19 14:07:28 +0000

    add database

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    num_guests = models.PositiveIntegerField()
+
+class Table(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    table_number = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
